3fe246008443d12a2eaf7ff69a5b2701687d32c5
Vinnicius Pereira <pereiradasilva0@hotmail.com>
2021-01-09 16:23:45 -0300

    Initial Commit

---
+++ Churras List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Churras List
+//
+//  Created by Vinnicius Pereira on 09/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
