65270a965afa39cd9d2290f8a6ddc6277bab8600
Vinnicius Pereira <pereiradasilva0@hotmail.com>
2021-03-03 15:26:06 -0300

    Final project.

---
+++ Churras List/Model/Others.swift
@@ -0,0 +1,19 @@
+//
+//  Others.swift
+//  Churras List
+//
+//  Created by Vinnicius Pereira on 21/01/21.
+//
+
+import Foundation
+import UIKit
+
+struct Others{
+    var name: String
+    var price: Int
+    var image: UIImage
+    var firstOpt: String? = ""
+    var sncOpt: String? = ""
+    var shouldHaveSegment: Bool? = false
+    var isMarked: Bool
+}
